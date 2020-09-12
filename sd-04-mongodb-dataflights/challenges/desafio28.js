96cd7d7ed609c082bc6cacc2cbec3e8241fed2c5
juniomelos <juniomelos@gmail.com>
2020-09-12 00:16:24 -0300

    project mongodb-dataflights

---
+++ sd-04-mongodb-dataflights/challenges/desafio28.js
@@ -0,0 +1,21 @@
+// Retorne a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.
+
+// totalFlights = db.voos.find({
+//   "empresa.nome": "LATAM AIRLINES BRASIL",
+//   natureza: "Doméstica"
+// }).count();
+
+db.resumoVoos.insert({
+  "empresa": "LATAM AIRLINES BRASIL",
+  "totalVoosDomesticos": db.voos.count({
+    "empresa.nome": "LATAM AIRLINES BRASIL",
+    natureza: "Doméstica"
+  }),
+});
+
+db.resumoVoos.find({
+  empresa: "LATAM AIRLINES BRASIL"
+  },{
+    _id: 0
+    }
+).limit(1);
