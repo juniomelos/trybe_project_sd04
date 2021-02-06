43767e9c4e4e2d5e28a9d0e3d514a745b3a54cd2
juniomelos <juniomelos@gmail.com>
2021-02-06 14:22:10 -0300

    projects->trybeer/tech-news/roject-exploring-the-protocols/inventory-report

---
+++ sd-04-trybeer/front-end/src/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import jwtDecode from 'jwt-decode';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loadFromLocalStorage } from './services/localStorage';
+import Login from './pages/Login/Login';
+import { login } from './store/ducks/user';
+import Register from './pages/Register/Register';
+import Products from './pages/Products/Products';
+import Checkout from './pages/Checkout/Checkout';
+import ClientOrders from './pages/ClientOrders/ClientOrders';
+import ClientProfile from './pages/ClientProfile/ClientProfile';
+import OrderDetail from './pages/OrderDetail/OrderDetail';
+import './App.css';
+import AdminProfile from './pages/AdminProfile';
+import Orders from './pages/Orders';
+
+function App() {
+  const dispatch = useDispatch();
+
+  const numDivisionDate = 1000; // eslint remote
+
+  const requireAuth = () => {
+    const userData = loadFromLocalStorage('user');
+
+    if (userData != null) {
+      const decoded = jwtDecode(userData.token);
+      const now = Date.now().valueOf() / numDivisionDate; // inspiration from web Stackflow
+      if (typeof decoded.exp !== 'undefined' && decoded.exp > now) {
+        const user = {
+          id: userData.id,
+          name: userData.name,
+          email: userData.email,
+          role: userData.role,
+        };
+        dispatch(login({ token: userData.token, data: user }));
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/register" component={ Register } />
+        <Route path="orders/:id" component={ OrderDetail } />
+        <Route
+          exact
+          path="/orders"
+          render={ () => requireAuth() ? <ClientOrders /> : <Redirect to="/login" /> }
+        />
+        <Route
+          exact
+          path="/products"
+          render={ () => requireAuth() ? <Products /> : <Redirect to="/login" /> }
+        />
+        <Route
+          exact
+          path="/checkout"
+          render={ () => requireAuth() ? <Checkout /> : <Redirect to="/login" /> }
+        />
+        <Route
+          exact
+          path="/profile"
+          render={ () => requireAuth() ? <ClientProfile /> : <Redirect to="/login" /> }
+        />
+        <Route
+          exact
+          path="/admin/orders"
+          render={ () => (requireAuth() ? <Orders /> : <Redirect to="/login" />) }
+        />
+        <Route
+          exact
+          path="/admin/profile"
+          render={ () => requireAuth() ? <AdminProfile /> : <Redirect to="/login" /> }
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
